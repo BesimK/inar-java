5a7b886f5f6e50aefc045f4073cd35b8d84746a5
BesimK <besimkarabiyikk@gmail.com>
2023-12-08 09:45:53 +0300

    Q Engineer!

--- src/QA/week_14/Question_14_08/Question_14_08.java
+++ src/QA/week_14/Question_14_08/Question_14_08.java
@@ -1,15 +1,11 @@
 package QA.week_14.Question_14_08;
 
-import QA.week_13.Question_13_08.MyStack;
-
-import java.util.Stack;
-
 public class Question_14_08 {
     public static void main(String[] args) {
-        MyStack stack = new MyStack();
+       NeoStack stack = new NeoStack();
 
         try {
-            System.out.println("Top method :" + stack.pop());
+            System.out.println("Top method :" + stack.top());
 
             System.out.println(stack.pop());
             System.out.println(stack.pop());
